1c62ad6f3f248ef96dd2b07dc5e2a3342e0799c0
Avijit Acharjee <avijitach@gmail.com>
2022-04-25 23:26:54 +0600

    fly game added

---
+++ app/src/main/java/com/avijit/bornomala/FlyDashboardActivity.java
@@ -0,0 +1,14 @@
+package com.avijit.bornomala;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class FlyDashboardActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_fly_dashboard);
+    }
+}
